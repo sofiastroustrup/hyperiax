826c2b4aeb08301a827600d62c972dd3c4908408
sofiastroustrup <sstroustrup@gmail.com>
2025-02-13 09:59:10 +0100

    added metropolis-hastings function which allows us to look at paths

--- hyperiax/mcmc/metropolis_hastings.py
+++ hyperiax/mcmc/metropolis_hastings.py
@@ -55,3 +55,57 @@ def metropolis_hastings(log_target, proposal, data, init, num_samples, burn_in=0
     print(f"Acceptance rate: {acceptance_rate:.4f}")
 
     return log_likelihoods,samples
+
+
+def metropolis_hastings_(log_target,proposal, data, init, num_samples, burn_in=0, thin=1, rng_key=None, savef=lambda x: x):
+    """ MCMC using Metropolis-Hastings algorithm
+
+    :param log_target: Function returning the logarithm of the target distribution
+    :param proposal: Function returning a sample from the proposal distribution
+    :param data: Observed data
+    :param init: Initial state
+    :param num_samples: Number of samples to draw
+    :param burn_in: Number of samples to discard at the beginning
+    :param thin: Thinning rate
+    :param rng_key: Random number generator key
+    :param savef: Function to save the state
+    :return: List of log likelihoods and samples
+    """
+
+    if rng_key is None:
+        rng_key = jax.random.PRNGKey(0)
+
+    rng_key, subkey = jax.random.split(rng_key)
+    current_state = init #proposal(data, init, subkey)
+    log_likelihood_current, path = log_target(data,current_state)
+    log_likelihoods = []
+    samples = []
+    paths = []
+    accepted_count = 0  # Initialize counter for accepted proposals
+
+    for i in tqdm(range(num_samples + burn_in)):
+        rng_key, subkey = jax.random.split(rng_key)
+        # Propose a new state, new noise, new parameters
+        proposed_state = proposal(data, current_state, subkey)
+
+        # Compute acceptance probability
+        log_likelihood_proposed, cur_path = log_target(data,proposed_state)
+        alpha = jnp.minimum(1.0, jnp.exp(log_likelihood_proposed - log_likelihood_current))
+
+        # Decide whether to accept the proposed state
+        if jax.random.uniform(subkey) < alpha:
+            current_state = proposed_state
+            log_likelihood_current = log_likelihood_proposed
+            accepted_count += 1  # Increment count if proposal is accepted
+
+        # Save the sample after burn-in and according to thinning rate
+        if i >= burn_in and (i - burn_in) % thin == 0:
+            log_likelihoods.append(log_likelihood_current)
+            samples.append(savef(current_state))
+            paths.append(cur_path)
+
+    # acceptance rate
+    acceptance_rate = accepted_count / (num_samples + burn_in)
+    print(f"Acceptance rate: {acceptance_rate:.4f}")
+
+    return log_likelihoods,samples, paths
